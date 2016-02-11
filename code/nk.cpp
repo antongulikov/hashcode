74cb2074d44cd8008406b51de1875829aa3d0c77
nk-karpov <nk.karpov@yandex.ru>
2016-02-11 21:57:03 +0300

    as

--- code/nk.cpp
+++ code/nk.cpp
@@ -1,2 +1,82 @@
-#include <cstdio>
-int main(){}
+#include <vector>
+
+using namespace std;
+
+int X, Y, D;
+int DL, ML;
+int P, W;
+int C;
+
+struct Warehouse {
+  int x, y;
+  vector<int> cap;
+
+  void read() {
+    scanf("%d%d", &x, &y);
+    cap = vector<int>(P);
+    for (int i = 0; i < P; i++) {
+      scanf("%d", &cap[i]);
+    }
+  }
+};
+
+struct Customer {
+  int x, y;
+  vector<int> l;
+  void read() {
+    scanf("%d%d", &x, &y);
+    int l_;
+    scanf("%d", &l_);
+    l = vector<int>(l_);
+    for (int i = 0; i < l_; i++) {
+      scanf("%d", &l[i]);
+    }
+  }
+};
+
+struct Action {
+  int t;
+
+};
+struct Drone {
+  int x, y;
+  int f;
+  int t;
+  vector<int> l;
+  vector<Action> act;
+  Drone(Warehouse w) {
+    x = w.x;
+    y = w.y;
+    t = 0;
+    f = 0;
+    l = vector<int>();
+  }
+};
+
+vector<int> weight;
+vector<Warehouse> wh;
+vector<Customer> cust;
+vector<Drone> dr;
+
+
+int main() {
+  scanf("%d%d", &X, &Y);
+  scanf("%d", &D);
+  scanf("%d%d", &DL, &ML);
+  scanf("%d", &P);
+  weight = vector<int>(P, 0);
+  for (int i = 0; i < P; i++) {
+    scanf("%d", &weight[i]);
+  }
+  scanf("%d", &W);
+  for (int i = 0; i < W; i++) {
+    wh[i].read();
+  }
+  scanf("%d", &C);
+  for (int i = 0; i < C; i++) {
+    cust[i].read();
+  }
+  for (int i = 0; i < D; i++) {
+    dr[i] = Drone(wh[0]);
+  }
+}
